ddd5e1bb7f7205187f248a4ae53c0fa76beb9283
lbv_o <91362451+gromlbv@users.noreply.github.com>
2024-10-11 17:45:53 +0300

    upd

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const user = window.Telegram.WebApp.initDataUnsafe.user;
-console.log(user);
-document.getElementsByClassName(h1).innerHTML = user;
+
+try{
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    console.log(user);
+    document.getElementById("test").innerHTML = user;
+} else{
+}
+
+
+try {
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    console.log(user);
+    
+    // Проверяем, существует ли пользователь и имеет ли он данные
+    if (user) {
+        document.getElementById("test").innerHTML = JSON.stringify(user); // Преобразуем объект в строку для отображения
+    } else {
+        document.getElementById("test").innerHTML = "aa"; // Сообщение, если пользователь не существует
+    }
+} catch (error) {
+    document.getElementById("test").innerHTML = "Ошибка";
+}
