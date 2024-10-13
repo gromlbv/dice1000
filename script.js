7bdf5bf6c60be21f7d91697bb0ab0b8e5c444f30
lbv_o <91362451+gromlbv@users.noreply.github.com>
2024-10-13 19:31:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
-//try {
-//    const user = window.Telegram.WebApp.initDataUnsafe.user;
-//    console.log(user);
-//    
-//    // Проверяем, существует ли пользователь и имеет ли он данные
-//    if (user) {
-//        document.getElementById("test").innerHTML = JSON.stringify(user); // Преобразуем объект в строку для отображения
-//    } else {
-//        document.getElementById("test").innerHTML = "aa"; // Сообщение, если пользователь не существует
-//    }
-//} catch (error) {
-//    document.getElementById("test").innerHTML = "Ошибка";
-//}
-//const user = window.Telegram.WebApp.initDataUnsafe.user;
-//console.log(user);
-//
-//if (user && user.photo_url) {
-//    // Получаем URL фото профиля
-//    const profilePhotoUrl = user.photo_url;
-//
-//    // Заменяем источник изображения
-//    document.getElementById("profile-pic").src = profilePhotoUrl;
-//} else {
-//    console.log("Пользователь не имеет фото профиля");
-//    document.getElementById("profile-pic").src = "default-profile-pic.png"; // Укажите путь к изображению по умолчанию
-//}
+try {
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    console.log(user);
+    
+    // Проверяем, существует ли пользователь и имеет ли он данные
+    if (user) {
+        document.getElementById("test").innerHTML = JSON.stringify(user); // Преобразуем объект в строку для отображения
+    } else {
+        document.getElementById("test").innerHTML = "aa"; // Сообщение, если пользователь не существует
+    }
+} catch (error) {
+    document.getElementById("test").innerHTML = "Ошибка";
+}
+const user = window.Telegram.WebApp.initDataUnsafe.user;
+console.log(user);
+
+if (user && user.photo_url) {
+    // Получаем URL фото профиля
+    const profilePhotoUrl = user.photo_url;
+
+    // Заменяем источник изображения
+    document.getElementById("profile-pic").src = profilePhotoUrl;
+} else {
+    console.log("Пользователь не имеет фото профиля");
+    document.getElementById("profile-pic").src = "default-profile-pic.png"; // Укажите путь к изображению по умолчанию
+}
 window.addEventListener("load", function() {
     const fadeInElements = document.querySelectorAll(".fade-in");
 
